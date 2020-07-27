3bf5c66e38e15bcdb0dda7abc6b61f7df55755b8
Bruno Souza <souza.brunoj@gmail.com>
2020-07-27 16:10:51 -0300

    Update libraries.

--- domain/src/main/java/br/com/bruno/domain/di/DomainModule.kt
+++ domain/src/main/java/br/com/bruno/domain/di/DomainModule.kt
@@ -2,15 +2,12 @@ package br.com.bruno.domain.di
 
 import br.com.bruno.domain.usecases.GetJobsUseCases
 import io.reactivex.schedulers.Schedulers
-import org.koin.dsl.module.module
+import org.koin.dsl.module
 
 val useCaseModule = module {
 
     factory {
-        GetJobsUseCases(
-            repository = get(),
-            scheduler = Schedulers.io()
-        )
+        GetJobsUseCases(repository = get(), scheduler = Schedulers.io())
     }
 }
 
